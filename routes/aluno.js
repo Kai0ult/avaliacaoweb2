33f357c4e7db0356da72067d9f2d28b4ee71d00b
Kai0ult <kaioult00@gmail.com>
2025-02-03 22:03:24 -0300

    ADD rotas certas

--- routes/aluno.js
+++ routes/aluno.js
@@ -2,7 +2,7 @@ import express from 'express'
 import AlunoController from '../controllers/AlunoController.js'
 const router = express.Router()
 
-router.get('/aluno', AlunoController.index)
+router.get('/', AlunoController.index)
 router.get('/cadastrar', AlunoController.cadastrar)
 router.post('/salvar', AlunoController.salvar)
 
